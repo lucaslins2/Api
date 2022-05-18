bbac487f7ce8c7402585eeea43c280f5aa87aa9d
FelipeLaus <felipe.laus10@gmail.com>
2022-05-17 22:36:05 -0300

    Update salvarPaginaAtual.php

--- Administracao_Cantina/php/Generico/salvarPaginaAtual.php
+++ Administracao_Cantina/php/Generico/salvarPaginaAtual.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'iniciarSessao.php';
 
-if($_GET['urlEndereco'] == "Generico/sair.php"){
+if($_GET['urlEndereco'] == 'Generico/php/sair.php'){
     $_SESSION['urlEndereco'] = 'Generico/php/login.php';
 }else{
     $_SESSION['urlEndereco'] = $_GET['urlEndereco'];
