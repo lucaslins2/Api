c8bf543f382786bd56248d4512cf9d3fc7a0294c
lucasyu01 <lucasyu01@gmail.com>
2022-05-28 20:34:18 -0300

    Carga Inicial Api PHP

---
+++ README.md
@@ -0,0 +1,7 @@
+PROJETO API PARA MOBILE LOGIN E CADASTRO, E HISTORICO
+ENPOINTS
+
+LOGIN
+http://localhost/api/User/login.php?username=shehryar&password=12345
+CADASTRO
+http://localhost/api/User/signup.php?username=admin2&email=lucas@gmail.com&password=12345
---
+++ User/signup.php
@@ -0,0 +1,37 @@
+<?php
+ 
+// get database connection
+include_once '../config/database.php';
+ 
+// instantiate user object
+include_once '../objects/user.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$user = new User($db);
+ 
+// set user property values
+$user->username = $_GET['username'];
+$user->email = $_GET['email'];
+$user->password = base64_encode($_GET['password']);
+$user->created = date('Y-m-d H:i:s');
+ 
+// create the user
+if($user->signup()){
+    $user_arr=array(
+        "status" => true,
+        "message" => "Successfully Signup!",
+        "id" => $user->id,
+        "username" => $user->username,
+        "email" => $user->email,
+    );
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "Username already exists!"
+    );
+}
+print_r(json_encode($user_arr));
+?>
